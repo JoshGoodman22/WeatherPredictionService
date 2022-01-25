405836de86b117e2795f0a3f6535de65c7dc3f77
JoshGoodman22 <jgoodman2024@chadwickschool.org>
2022-01-24 21:18:45 -0800

    Feat pushed work

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace WeatherPredictionService
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Tim lin is silly ");
         }
     }
 }

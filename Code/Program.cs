ba52b267ff61accdf87c7782dac1c1e9104d1b2d
JoshGoodman22 <jgoodman2024@chadwickschool.org>
2022-01-24 22:14:27 -0800

    Chore: Costumer can now enter a date

---
+++ Code/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace WeatherPredictionService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Month;
+            string Day;
+
+            Console.WriteLine("Hello, I am your weather Prediction service - Please enter a month as an integer 1-12 for the month");
+            Month = Console.ReadLine();
+            Console.WriteLine($"Okay awesome, this is month {Month} of the year. Now please enter a day from that month as an integer 1-31");
+            Day = Console.ReadLine();
+            Console.WriteLine($"Okay awesome, you want to know the weather for the date {Month}/{Day}");
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# WeatherPredictionService
+# Weather Prediction Service Personal Project 
+## Flowchart Layouts
+### High level layout
+![Image Text](HighLevelOV.png)
+### Initial Question loop 
+![Image Text](Iniital.png)
+### Sting High level Flowchart
+![Image Text](ListOV.png)

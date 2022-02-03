56d10bf597099be836ad59d0c8661f49362f320d
JoshGoodman22 <jgoodman2024@chadwickschool.org>
2022-02-03 08:21:30 -0800

    Update TestGetStarted.cs

--- TestFolder/TestGetStarted.cs
+++ TestFolder/TestGetStarted.cs
@@ -12,7 +12,7 @@ public static bool RunTest()
 // In this Test, I would like to create test to see if the user entered a a date. As long as they entered just about anything, it would go into the validate date method as an int. I would then test that the stored variable of the user matches the readline of the users responce. 
     {
      //(int UserMonth, int Userday) = WeatherPredictionService.GettingDate.GetStarted("09","21")  ** WHy wont this work. My gettingDate is all screwed up. 
-     i
+     
 
         return false;
     }

1bf97384d7dc7b89fcc08f8d1cbd12ad209e830c
JoshGoodman22 <jgoodman2024@chadwickschool.org>
2022-02-02 22:32:39 -0800

    Added notes for Mx. Collard for the morning ofFeb3

--- TestFolder/TestCreatePrediction.cs
+++ TestFolder/TestCreatePrediction.cs
@@ -10,6 +10,9 @@ namespace WeatherPredictionService
         public static bool RunTest()
         {
 
+                // Note for Mx. Collard - In this test I wold have made sure of the folling
+                // 1. That the numbers of Average Temp are real- non negative numbers that can be converted into an average. I would also make sure that these match the different dates the came from and that each date is the same. I would also test the method to see what happens if a date is in the list instead of a Kelvin number. Make sure that there were no errors complining the list. 
+            
             //This Method will use the temperatures From FilterDates() to create an average, mode, mean, and median. It will then display the information to the user
             // This methos will take in and read the string of temperatures from GetTemperatures 
 
--- TestFolder/TestGetStarted.cs
+++ TestFolder/TestGetStarted.cs
@@ -9,10 +9,10 @@ namespace WeatherPredictionService
 class TestGetStarted
 {
 public static bool RunTest()
-// This method should produce two
+// In this Test, I would like to create test to see if the user entered a a date. As long as they entered just about anything, it would go into the validate date method as an int. I would then test that the stored variable of the user matches the readline of the users responce. 
     {
      //(int UserMonth, int Userday) = WeatherPredictionService.GettingDate.GetStarted("09","21")  ** WHy wont this work. My gettingDate is all screwed up. 
-     int st
+     i
 
         return false;
     }
